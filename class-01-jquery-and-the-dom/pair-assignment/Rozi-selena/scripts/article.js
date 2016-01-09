7e61a6ac1b7e6e0a503470df7e2fd27f5bcd1f43
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-08 16:01:09 -0800

    correct code to show the data on page

--- class-01-jquery-and-the-dom/pair-assignment/Rozi-selena/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/Rozi-selena/scripts/article.js
@@ -12,26 +12,27 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  //
+    $newArticle.removeClass('template');
 
-  $newArticle.data('category', this.category);
-  $newArticle.data('title',this.title);
-  $newArticle.data('author',this.author);
-  $newArticle.data('authorUrl',this.authorUrl);
-  $newArticle.data('publishedOn',this.publishedOn);
-  $newArticle.data('body',this.body);
-
+  // $newArticle.data('category', this.category);
+  // $newArticle.data('title',this.title);
+  // $newArticle.data('author',this.author);
+  // $newArticle.data('authorUrl',this.authorUrl);
+  // $newArticle.data('publishedOn',this.publishedOn);
+  // $newArticle.data('body',this.body);
+     $newArticle.find('address a').html(this.author);
+     $newArticle.find('address a').attr('href',this.authorUrl);
+     $newArticle.find('h1').html(this.title);
+     $newArticle.find('.article-body').html(this.body);
 
   // Include the publication date as a 'title' attribute to show on hover:
     $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
 
-
   // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-
-  $newArticle.append('<hr>');
-
-$newArticle.removeClass('template');
-  return $newArticle;
+    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
+    $newArticle.append('<hr>');
+    return $newArticle;
 }
 
 rawData.sort(function(a,b) {
