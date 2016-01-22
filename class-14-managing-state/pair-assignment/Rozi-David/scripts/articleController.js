10c6e847da6823f6b2395a7624a63c72ba3172d9
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-21 22:04:53 -0800

    articleView Todos Comment

--- class-14-managing-state/pair-assignment/Rozi-David/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/Rozi-David/scripts/articleController.js
@@ -17,23 +17,29 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
+
   // TODO: Middleware for loading up articles by a certain author:
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
       next();
     };
-
+    //
     Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
   };
 
   // TODO: Middleware for grabbing all articles with a certain category:
+  //comment:
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
-      next();
+      next(); // cooment: run the articlesController.index
     };
-
+    //comment: findWhere is a method with a function that has 3 parameters(field,value and callback function)
+    //in this part Article.findWhere() return data base on category filed from database and then if
+    //category = ctx.params.categoryName ( that is value of category and give it from part of url like '/category/:categoryName' )
+    //and return return the callback function (categoryData) that this function set the ctx(context).allArticles
+    //property  = articlesInCategory
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
